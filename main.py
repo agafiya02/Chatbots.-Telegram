759dfe77095ea3d4905ce56917c2a5e700e44fea
agafiya02 <151033863+agafiya02@users.noreply.github.com>
2024-03-25 18:13:58 +0500

    Дата и время

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
-from telegram.ext import Application, MessageHandler, filters
+from telegram.ext import Application, MessageHandler, filters, CommandHandler
+import datetime
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG
@@ -12,11 +13,34 @@ async def echo(update, context):
     await update.message.reply_text(f"Я получил сообщение {update.message.text}")
 
 
+async def start(update, context):
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Привет {user.mention_html()}! Я эхо-бот. Напишите мне что-нибудь, и я пришлю это назад!",
+    )
+
+
+async def help_command(update, context):
+    await update.message.reply_text("Я пока не умею помогать... Я только ваше эхо.")
+
+
+async def time(update, context):
+    now = datetime.datetime.now()
+    await update.message.reply_text(now.strftime('%H:%M'))
+
+
+async def date(update, context):
+    now = datetime.datetime.now()
+    await update.message.reply_text(now.strftime("%Y-%m-%d"))
+
+
 def main():
     application = Application.builder().token('').build()
-
-    text_handler = MessageHandler(filters.TEXT, echo)
-
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("time", time))
+    application.add_handler(CommandHandler("date", date))
+    text_handler = MessageHandler(filters.TEXT & ~filters.COMMAND, echo)
     application.add_handler(text_handler)
     application.run_polling()
 
